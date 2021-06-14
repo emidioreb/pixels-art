a8537e5cfc4a2f101155464b5bbe5a85b9b2df1b
Emídio Rebouças <emidioreb@gmail.com>
2021-06-14 11:52:51 -0300

    start project

---
+++ script.js
@@ -0,0 +1,34 @@
+const colors = document.querySelectorAll('.color');
+const palette = document.querySelector('#color-palette');
+
+palette.addEventListener('click', function (event){
+  if(event.target.classList.contains('color')){
+    colors.forEach(function(_, index){
+      colors[index].classList.remove("selected");
+    })
+    event.target.classList.add("selected");
+  }
+})
+//
+const pixelColor = document.querySelectorAll(".pixel");
+
+function print(){
+  for (let index = 0; index < colors.length; index += 1) {
+    if(colors[index].classList.contains('selected')){
+    let getPalette = colors[index];
+    let backGround = window.getComputedStyle(getPalette, null).getPropertyValue("background-color");
+    return backGround;
+  }
+  }
+}
+
+function printB(event){
+  event.target.style.backgroundColor = print()
+}
+
+function printC(){
+  for(let index = 0; index < pixelColor.length; index += 1){
+    pixelColor[index].addEventListener('click', printB)
+  }
+}
+printC();
